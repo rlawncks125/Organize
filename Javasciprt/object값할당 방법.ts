c38b4d07661bf71bf2db1235bc6e5849894ee624
김주찬 <rlawncks125@naver.com>
2021-11-30 12:47:35 +0900

    .2

--- Javasciprt/object값할당 방법.ts
+++ Javasciprt/object값할당 방법.ts
@@ -23,7 +23,7 @@ ds = { ...ds, ...newAdd };
 // { ssa: 'sww', add: 'sas', keyKK: 'valeee' }
 console.log(ds);
 
-// ------------------
+// ----------------  출력
 console.log(ds.ssa);
 
 console.log(ds["add"]);
@@ -32,6 +32,7 @@ console.log(ds[`${Key}`]);
 
 // 타입 검사 or 특정 프로퍼티 있는지 검사
 if (typeof ds === "object" && ds.hasOwnProperty("www")) {
+  // 출력안함
   console.log(ds.ssa);
 }
 
