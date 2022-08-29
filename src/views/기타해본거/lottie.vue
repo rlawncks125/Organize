f12880fb1df17d5a2e617421d897794ffdd29800
김주찬 <rlawncks125@naver.com>
2022-08-30 02:50:00 +0900

    lottie 컨트롤러 기능

--- src/views/기타해본거/lottie.vue
+++ src/views/기타해본거/lottie.vue
@@ -1,6 +1,13 @@
 <template>
-  <input type="range" min="0" max="100" v-model="lottieRange" />
+  <input
+    @input="onClickedMoveRange"
+    type="range"
+    min="0"
+    max="99"
+    v-model="lottieRange"
+  />
   <span>{{ lottieRange }}</span>
+  <button @click="play">다시시작</button>
 
   <div ref="lottieRef"></div>
 </template>
@@ -14,17 +21,36 @@ const lottieRef = ref<HTMLElement>();
 const lottieRange = ref(0);
 
 let lottie: AnimationItem;
+let progressObs: number;
+
+const 진행률 = () => {
+  if (!lottie) return;
+  lottieRange.value = Math.floor(
+    (lottie.currentFrame / lottie.totalFrames) * 100
+  );
+};
+
+const play = () => {
+  lottie.play();
+
+  progressObs = setInterval(진행률, 10);
+};
+
 onMounted(() => {
   lottie = lottieLoadAnimation(lottieRef.value!, "box");
-  console.log(lottie);
+
+  progressObs = setInterval(진행률, 10);
 });
 
-watch(lottieRange, () => {
+const onClickedMoveRange = () => {
+  console.log("range click", lottieRange.value);
+  if (!lottie) return;
+  clearInterval(progressObs);
+
+  // frame을 계산하여 ragne값 인 진행률만큼 이동
   const frame = lottie.totalFrames / (lottie.frameRate / 10);
-
-  console.log((lottie.currentFrame / lottie.totalFrames) * 100);
   lottie.goToAndStop(lottieRange.value * frame);
-});
+};
 </script>
 
 <style scoped></style>
